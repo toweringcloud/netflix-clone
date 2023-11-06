05773ff216f7ab72e77d335fa7db197e134d12dd
toweringcloud <toweringcloud@gmail.com>
2023-11-06 19:29:41 +0900

    [ADD] slider pages below banner title

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 
@@ -80,7 +80,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 		<QueryClientProvider client={queryClient}>
 			<RecoilRoot>
 				<ThemeProvider theme={theme}>
-					<ReactQueryDevtools initialIsOpen={true} />
+					{/* <ReactQueryDevtools initialIsOpen={true} /> */}
 					<GlobalStyle />
 					<App />
 				</ThemeProvider>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import Loader from "../components/Loader";
@@ -8,23 +10,19 @@ import { makeImagePath } from "../utils";
 const Wrapper = styled.div`
 	width: 100%;
 	height: 100%;
-	margin-top: 15vh;
+	margin-top: 50vh;
 	background-color: black;
 	color: "white";
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
 `;
 
-const Banner = styled.div<{ bgPhoto: string }>`
+const Banner = styled.div<{ bgphoto: string }>`
 	height: 100vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	padding: 60px;
 	background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-		url(${(props) => props.bgPhoto});
+		url(${(props) => props.bgphoto});
 	background-size: cover;
 `;
 
@@ -37,25 +35,107 @@ const Overview = styled.p`
 	width: 50%;
 `;
 
+const Slider = styled.div`
+	position: relative;
+	top: -100px;
+`;
+const Row = styled(motion.div)`
+	position: absolute;
+	width: 100%;
+	display: grid;
+	gap: 5px;
+	grid-template-columns: repeat(6, 1fr);
+`;
+const rowVariants = {
+	hidden: {
+		x: window.outerWidth + 5,
+	},
+	visible: {
+		x: 0,
+	},
+	exit: {
+		x: -window.outerWidth - 5,
+	},
+};
+
+const Box = styled(motion.div)<{ bgphoto: string }>`
+	background-color: white;
+	background-image: url(${(props) => props.bgphoto});
+	background-size: cover;
+	background-position: center center;
+	height: 200px;
+	font-size: 66px;
+`;
+
+// visible movie count per page
+const offset = 6;
+
 function Home() {
 	const { data, isLoading } = useQuery<IGetMoviesResult>({
 		queryKey: ["movies", "nowPlaying"],
 		queryFn: getMovies,
 	});
 
+	const [index, setIndex] = useState(0);
+	const [leaving, setLeaving] = useState(false);
+	const increaseIndex = () => {
+		if (data) {
+			if (leaving) return;
+			toggleLeaving();
+			const totalMovies = data.results.length - 1;
+			const maxIndex = Math.floor(totalMovies / offset) - 1;
+			setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+		}
+	};
+	const toggleLeaving = () => setLeaving((prev) => !prev);
+
 	return (
 		<Wrapper>
 			{isLoading ? (
 				<Loader />
 			) : (
-				<Banner
-					bgPhoto={makeImagePath(
-						data?.results[0].backdrop_path || ""
-					)}
-				>
-					<Title>{data?.results[0].title}</Title>
-					<Overview>{data?.results[0].overview}</Overview>
-				</Banner>
+				<>
+					<Banner
+						onClick={increaseIndex}
+						bgphoto={makeImagePath(
+							data?.results[0].backdrop_path || ""
+						)}
+					>
+						<Title>{data?.results[0].title}</Title>
+						<Overview>{data?.results[0].overview}</Overview>
+					</Banner>
+					<Slider>
+						<AnimatePresence
+							initial={false}
+							onExitComplete={toggleLeaving}
+						>
+							<Row
+								variants={rowVariants}
+								initial="hidden"
+								animate="visible"
+								exit="exit"
+								transition={{ type: "tween", duration: 1 }}
+								key={index}
+							>
+								{data?.results
+									.slice(1)
+									.slice(
+										offset * index,
+										offset * index + offset
+									)
+									.map((movie) => (
+										<Box
+											key={movie.id}
+											bgphoto={makeImagePath(
+												movie.backdrop_path,
+												"w500"
+											)}
+										/>
+									))}
+							</Row>
+						</AnimatePresence>
+					</Slider>
+				</>
 			)}
 		</Wrapper>
 	);
