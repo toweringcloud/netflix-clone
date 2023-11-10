55fe896e27109a8b48b4e983f887d5b2c03a79bc
toweringcloud <toweringcloud@gmail.com>
2023-11-10 12:39:46 +0900

    [MOD] build size option up

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/netflix-clone",
+	build: { chunkSizeWarningLimit: 1024 },
 	plugins: [react()],
 });
